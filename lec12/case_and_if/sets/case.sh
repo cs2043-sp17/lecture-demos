039b20c04ccbd14db4ca3c5a9007472eb78b487a
sven <sjm324@cornell.edu>
2017-02-22 11:48:12 -0500

    fixing indentation

--- lec12/case_and_if/sets/case.sh
+++ lec12/case_and_if/sets/case.sh
@@ -3,7 +3,7 @@ case "$1" in
     [[:digit:]] )
         echo "$1 blargh echoes..."
         for (( i = 1; i <= $1; i++ )); do
-        echo " [$i] blargh"
+            echo " [$i] blargh"
         done
         ;;
     * )
